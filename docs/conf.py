52f1baf6cbf4456569c6ed16d2b1d0ac09c5191f
Matthew Hill <mhill@stsci.edu>
2016-12-19 12:54:44 -0500

    use ipython

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ import os
 # ones.
 extensions = [
     'nbsphinx',
+    'IPython.sphinxext.ipython_console_highlighting',
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
