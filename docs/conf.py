bb7d4d92087d5857142d96d2367413ffbae9b906
Matthew Hill <mattjhill@users.noreply.github.com>
2016-12-19 15:03:31 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-#    'nbsphinx',
+    'nbsphinx',
     'IPython.sphinxext.ipython_console_highlighting',
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
